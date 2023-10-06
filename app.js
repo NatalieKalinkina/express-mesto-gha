12cf68cc7f9d15685c37bec87648919468cdc53c
Natalie Kalinkina <ms.nataly.r@yandex.ru>
2023-10-06 17:34:21 +0400

    создать контроллеры и роуты для пользователей

--- app.js
+++ app.js
@@ -4,9 +4,21 @@ const mongoose = require('mongoose');
 const { PORT = 3000 } = process.env;
 
 const app = express();
+const userRouter = require('./routes/users');
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
+});
+app.use('/', userRouter);
+
+app.use((req, res, next) => {
+  req.user = { _id: '65200b0e226d52e48ed51ee4' };
+  next();
 });
 
 app.listen(PORT);
+
+// 65200b0e226d52e48ed51ee4
